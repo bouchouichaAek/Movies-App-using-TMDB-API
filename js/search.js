401a85a87a39eb59c02a335bee0a57bd269ff484
Bouchouicha abdelkader <lakamoraaek@gmail.com>
2022-12-24 19:50:52 +0100

    Delete search.js

--- js/search.js
+++
@@ -1,7 +0,0 @@
-fetch(
-  "https://api.themoviedb.org/3/search/movie?api_key=ee9ddd028297c7c00ad6168b72365519&query='"
-)
-  .then((response) => response.json())
-  .then(function (response) {
-    console.log(response);
-  });
